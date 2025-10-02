006e6276130d73ee8968f8f09b9ec4fd4583064d
jjtt48 <74917684+jjtt48@users.noreply.github.com>
2025-10-02 15:49:55 -0500

    Update csvfiles.py

--- GNU Parallel/csvfiles.py
+++ GNU Parallel/csvfiles.py
@@ -1,12 +1,10 @@
-docker run --rm -v "D:\U semestre 11\Pro paralela\Corte 2\Clase octubre 1\Tareas de la semana en clase\Actividad del miercoles":/work -w /work alhumaidyaroob/gnu-parallel ls
-# create_csv.py
 import csv
 import random
 
 def create_csv(filename, rows=1000000):
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["value1", "value2", "value3"])  # encabezados
+        writer.writerow(["value1", "value2", "value3"])  
         for _ in range(rows):
             writer.writerow([random.randint(0, 1000),
                              random.randint(0, 1000),
@@ -15,3 +13,4 @@ def create_csv(filename, rows=1000000):
 create_csv("data1.csv")
 create_csv("data2.csv")
 create_csv("data3.csv")
+
